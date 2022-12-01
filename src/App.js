2dd59d71c4b7d9d3ee0dc063e4018a9079b44430
Harsh Vardhan Singh Shekhawat <77687381+Harsh-Vardhan-Singh-Shekhawat@users.noreply.github.com>
2022-12-01 19:26:10 +0530

    add movie component added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, {useState} from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
+import AddMovie from './components/AddMovie';
 
 function App() {
   const [movies,setMovies] = useState([]);
   const [isLoading,setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
   function fetchMoviesHandler(){
     setIsLoading(true)
+    setError(null)
     fetch('https://swapi.dev/api/films/')
     .then(response =>{
       return response.json();
     })
-    .then(data =>{
+    .then(data => {
       const tranformedData = data.results.map(moviesData => {
         return {
         id:moviesData.episode_id,
@@ -23,14 +26,28 @@ function App() {
       setIsLoading(false);
     })
   }
+  async function addMovieHandler(movie) {
+    // console.log(movie)
+     const response = await fetch('https://movieworld-6cfd9-default-rtdb.firebaseio.com/movies.json' , {
+      method:'POST',
+      body:JSON.stringify(movie)
+    });
+
+    const data = await response.json();
+    
+  }
 
   return (
     <React.Fragment>
       <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
+      <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section> 
       <section>
-        {!isLoading && <MoviesList movies={movies} />}
+        {!isLoading && movies.length>0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length ===0&& <p>No movies found</p>}
         {isLoading && <p>Loading...</p>}
       </section>
       
